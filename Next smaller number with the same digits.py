6a7e5edbcbf1dd1f3d688803a264991b0f84f402
Doson <karbushev-1991@mail.ru>
2022-03-01 23:46:10 +0700

    тест комита

--- Next smaller number with the same digits.py
+++ Next smaller number with the same digits.py
@@ -1,4 +1,5 @@
 """
+4 kata
 Write a function that takes a positive integer and returns the next smaller positive integer containing the same digits.
 
 For example:
@@ -6,7 +7,8 @@ For example:
 next_smaller(21) == 12
 next_smaller(531) == 513
 next_smaller(2071) == 2017
-Return -1 (for Haskell: return Nothing, for Rust: return None), when there is no smaller number that contains the same digits. Also return -1 when the next smaller number with the same digits would require the leading digit to be zero.
+Return -1 (for Haskell: return Nothing, for Rust: return None), when there is no smaller number that contains the same
+digits. Also return -1 when the next smaller number with the same digits would require the leading digit to be zero.
 
 next_smaller(9) == -1
 next_smaller(135) == -1
@@ -14,3 +16,13 @@ next_smaller(1027) == -1  # 0721 is out since we don't write numbers with leadin
 some tests will include very large numbers.
 test data only employs positive integers.
 """
+
+def next_smaller(n):
+    if len(n) == 1:
+        return -1
+    if n[0] > 1:
+        pass
+
+if __name__ == '__main__':
+
+    next_smaller(531)
